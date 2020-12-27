655a7b244897428a18b25e6e93d6708f47e7820d
Benjamin Koch <bbbsnowball@gmail.com>
2020-12-27 06:49:03 +0000

    split config

---
+++ rockpro64.nix
@@ -0,0 +1,28 @@
+{ config, lib, pkgs, systems, ... }:
+let
+  linux_rock64_4_19 = pkgs.callPackage ./linux-rock64/4.19.nix {
+    kernelPatches = [ pkgs.kernelPatches.bridge_stp_helper ];
+  };
+  linux_rock64_4_20 = pkgs.callPackage ./linux-rock64/4.20.nix {
+    kernelPatches = [ pkgs.kernelPatches.bridge_stp_helper ];
+  };
+  linux_rock64_5_3 = pkgs.callPackage ./linux-rock64/5.3.nix {
+    kernelPatches = [ pkgs.kernelPatches.bridge_stp_helper ];
+  };
+
+  linuxPackages_rock64_4_19 = pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor linux_rock64_4_19);
+  linuxPackages_rock64_4_20 = pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor linux_rock64_4_20);
+  linuxPackages_rock64_5_3 = pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor linux_rock64_5_3);
+
+in
+{
+  boot = {
+    loader = {
+      grub.enable = false;
+      generic-extlinux-compatible.enable = true;
+    };
+    # kernelPackages = linuxPackages_rock64_5_3;
+    # kernelPackages = linuxPackages_rock64_4_20;
+    kernelPackages = linuxPackages_rock64_4_19;
+  };
+}
--- sd-image-aarch64-rockpro64.nix
+++ sd-image-aarch64-rockpro64.nix
@@ -1,36 +1,7 @@
-{ config, lib, pkgs, systems, ... }:
-let
-  linux_rock64_4_19 = pkgs.callPackage ./linux-rock64/4.19.nix {
-    kernelPatches = [ pkgs.kernelPatches.bridge_stp_helper ];
-  };
-  linux_rock64_4_20 = pkgs.callPackage ./linux-rock64/4.20.nix {
-    kernelPatches = [ pkgs.kernelPatches.bridge_stp_helper ];
-  };
-  linux_rock64_5_3 = pkgs.callPackage ./linux-rock64/5.3.nix {
-    kernelPatches = [ pkgs.kernelPatches.bridge_stp_helper ];
-  };
-
-  linuxPackages_rock64_4_19 = pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor linux_rock64_4_19);
-  linuxPackages_rock64_4_20 = pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor linux_rock64_4_20);
-  linuxPackages_rock64_5_3 = pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor linux_rock64_5_3);
-
-in
+{ ... }:
 {
   imports = [
     <nixpkgs/nixos/modules/installer/cd-dvd/sd-image-aarch64.nix>
+    ./rockpro64.nix
   ];
-  # put your own configuration here, for example ssh keys:
-  #users.extraUsers.root.openssh.authorizedKeys.keys = [
-  #   "ssh-ed25519 AAAAC3NzaC1lZDI1.... username@tld"
-  #];
-
-  boot = {
-    loader = {
-      grub.enable = false;
-      generic-extlinux-compatible.enable = true;
-    };
-    # kernelPackages = linuxPackages_rock64_5_3;
-    # kernelPackages = linuxPackages_rock64_4_20;
-    kernelPackages = linuxPackages_rock64_4_19;
-  };
 }
