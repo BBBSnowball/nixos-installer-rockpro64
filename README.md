7abf0c5065ac49a2801e14f454e73aecd53fec81
Benjamin Koch <bbbsnowball@gmail.com>
2020-12-27 05:55:57 +0000

    automatically add u-boot to SD image

--- README.md
+++ README.md
@@ -29,21 +29,5 @@ In the below commands replace mmcblkX with the correct sdcard device.
 
 ```
 # Copy the image to the sdcard
-sudo dd if=nixos-sd-image-19.03pre-git-aarch64-linux.img of=/dev/mmcblkX status=progress
-
-# Use fdisk to delete first partition. 
-# (The first partition currently contains firmware for other boards and is not useful for the rockpro64.
-#  It also gets in the way of uboot currently.)
-
-sudo fdisk /dev/mmcblkX
-# Delete first partition and write: d 1 w
-```
-
-Now copy the rockpro64 specific uboot build to the sdcard.
-The correct uboot build can be found here: <https://hydra.nixos.org/job/nixpkgs/trunk/ubootRockPro64.aarch64-linux>
-See <https://nixos.wiki/wiki/NixOS_on_ARM/PINE64_ROCKPro64> for more information.
-```
-# Copy the uboot image to the sdcard starting at block 64
-sudo dd if=idbloader.img of=/dev/mmcblkX status=progress bs=512 seek=64
-
+zstd -d <./result/sd-image/nixos-sd-image-*.img.zst | sudo dd of=/dev/mmcblkX bs=100M status=progress && sync
 ```
--- default.nix
+++ default.nix
@@ -5,5 +5,27 @@
 let
   evaluatedSystem = nixos { configuration = { imports = [ config1 ] ++ (if builtins.pathExists config2 then [ config2 ] else []); }; };
   sdImage1 = evaluatedSystem.config.system.build.sdImage;
+  inherit (evaluatedSystem) pkgs;
+  sdImage2 = pkgs.stdenv.mkDerivation {
+    name = "nixos-install-aarch64-rockpro64-with-uboot";
+    version = "0.1";
+    src = sdImage1;
+    buildInputs = [ pkgs.ubootRockPro64 ];
+    nativeBuildInputs = with pkgs; [ utillinux zstd ];
+    unpackPhase = ''
+      zstd -d <$src/sd-image/nixos-sd-image-*.img.zst >image
+    '';
+    buildPhase = ''
+      (echo d; echo 1; echo p; echo w) | fdisk ./image
+      dd if=${pkgs.ubootRockPro64}/idbloader.img of=image conv=fsync,notrunc bs=512 seek=64
+      dd if=${pkgs.ubootRockPro64}/u-boot.itb    of=image conv=fsync,notrunc bs=512 seek=16384
+    '';
+    installPhase = ''
+      mkdir $out $out/sd-image
+      cp $src/nix-support -r $out/
+      filename=$(cd $src/sd-image && ls -1 nixos-sd-image-*.img.zst)
+      zstd <image >$out/sd-image/"$filename"
+    '';
+  };
 in
-sdImage1
+sdImage2
