7ad3d7fc6a9f020947d3fa22c5191ce49e67a62f
Benjamin Koch <bbbsnowball@gmail.com>
2021-01-01 01:09:44 +0100

    switch baud rate to 1.5 MBaud
    ... to make it the same as in u-boot and boot ROM.

--- rockpro64.nix
+++ rockpro64.nix
@@ -2,6 +2,7 @@
 let
   kernels = import ./kernels.nix { inherit pkgs; };
   defaultKernelVersion = "4.19";  # or 4.20 or 5.3
+  baudRate = 1500000;  # 1.5 MBaud, also used by boot ROM and u-boot
 in
 {
   boot = {
@@ -10,8 +11,23 @@ in
       generic-extlinux-compatible.enable = true;
     };
     kernelPackages = lib.mkDefault kernels.linuxPackages_rock64.${defaultKernelVersion};
-    kernelParams = [ "console=ttyS2,115200n8" ];
+    kernelParams = [ "console=ttyS2,${toString baudRate}n8" ];
   };
 
   #services.journald.console = "ttyS2";
+
+  # This adds 1.5 MBaud for all ttys.
+  services.mingetty.serialSpeed = lib.mkOptionDefault (lib.mkBefore [ baudRate ]);
+
+  # I would like to set 1.5 MBaud for ttyS2 only. However, we would have to use an overrides.conf
+  # to not override all option of serial-getty@.service. NixOS will use an override.conf if a
+  # service with the same name already exists but this is not the case here because the upstream
+  # service has a different name (no instance suffix). We cannot use environment.etc either because
+  # the relevant directory points to a read-only store path.
+  #systemd.services."serial-getty@ttyS2".serviceConfig.ExecStart = [ "" "TODO" ];
+  #environment.etc."systemd/system/serial-getty@ttyS2.service.d/overrides.conf".text = ''
+  #  [Service]
+  #  ExecStart=
+  #  ExecStart=TODO
+  #'';
 }
